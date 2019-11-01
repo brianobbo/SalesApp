febb9664ed3936f6464fcd16a0e50a0ec6232b25
Bilal Rizwan <bilalbilal488@gmail.com>
2019-11-01 12:04:57 -0700

    code of login

--- app/src/main/java/com/technohem/salesapp/RegisterActivity.kt
+++ app/src/main/java/com/technohem/salesapp/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.technohem.salesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,6 +9,7 @@ import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -25,7 +27,17 @@ class RegisterActivity : AppCompatActivity() {
 
                 var rq:RequestQueue=Volley.newRequestQueue(this)
                 var sr = StringRequest(Request.Method.GET,url,Response.Listener { response ->
-                    Toast.makeText(this,response, Toast.LENGTH_LONG).show()
+                    if(response.equals("0"))
+                    {
+                        Toast.makeText(this,"Mobile Already Used", Toast.LENGTH_LONG).show()
+                    }
+                    else {
+                        Toast.makeText(this,"User Created Successfully", Toast.LENGTH_LONG).show()
+                        UserInfo.mobile=reg_mobile.text.toString()
+                        var i = Intent(this,HomeActivity::class.java)
+                        startActivity(i)
+                    }
+
                 },Response.ErrorListener { error ->
                     Toast.makeText(this,error.message, Toast.LENGTH_LONG).show()
                 })
---
+++ app/src/main/java/com/technohem/salesapp/UserInfo.kt
@@ -0,0 +1,10 @@
+package com.technohem.salesapp
+
+class UserInfo {
+
+    // companion / static because use it in different screen
+    companion object {
+        var mobile:String=""
+    }
+
+}
