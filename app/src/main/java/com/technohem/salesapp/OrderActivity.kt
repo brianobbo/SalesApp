b4eff6819c01f47f5b5e01bca6b7e26194d86321
Bilal Rizwan <bilalbilal488@gmail.com>
2019-11-02 16:02:44 -0700

    cancel order

--- app/src/main/java/com/technohem/salesapp/OrderActivity.kt
+++ app/src/main/java/com/technohem/salesapp/OrderActivity.kt
@@ -11,6 +11,7 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_order.*
 
@@ -55,6 +56,23 @@ class OrderActivity : AppCompatActivity() {
         {
             var i = Intent(this,HomeActivity::class.java)
             startActivity(i)
+        }
+
+        if(item?.itemId==R.id.item_cancel)
+        {
+            var url="http://192.168.0.109/SalesWeb/cancel_order.php?mobile=" + UserInfo.mobile
+
+            var rq:RequestQueue=Volley.newRequestQueue(this)
+            var sr= StringRequest(Request.Method.GET,url,Response.Listener { response ->
+
+                var i=Intent(this,HomeActivity::class.java)
+                startActivity(i)
+
+            },Response.ErrorListener { error ->
+                Toast.makeText(this,error.message,Toast.LENGTH_LONG).show()
+            })
+
+            rq.add(sr)
         }
 
         return super.onOptionsItemSelected(item)
