d6cdb1106c29afe40be2e32814ed279d60a0b4a0
Bilal Rizwan <bilalbilal488@gmail.com>
2019-11-01 19:20:34 -0700

    implement RecyclerView in ItemActivity

---
+++ app/src/main/java/com/technohem/salesapp/Item.kt
@@ -0,0 +1,18 @@
+package com.technohem.salesapp
+
+class Item {
+
+    var id:Int
+    var name:String
+    var price:String
+    var photo:String
+
+    constructor(id:Int,name:String,price:String,photo:String)
+    {
+        this.id=id
+        this.name=name
+        this.price=price
+        this.photo=photo
+    }
+
+}
--- app/src/main/java/com/technohem/salesapp/ItemActivity.kt
+++ app/src/main/java/com/technohem/salesapp/ItemActivity.kt
@@ -2,6 +2,14 @@ package com.technohem.salesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_item.*
 
 class ItemActivity : AppCompatActivity() {
 
@@ -10,7 +18,25 @@ class ItemActivity : AppCompatActivity() {
         setContentView(R.layout.activity_item)
 
         var cat:String = intent.getStringExtra("cat")
-        var url = "http://localhost/SalesWeb/get_items.php?category=$cat"
+        var url = "http://192.168.0.109/SalesWeb/get_items.php?category=$cat"
+        var list=ArrayList<Item>()
+
+        var rq: RequestQueue = Volley.newRequestQueue(this)
+        var jar= JsonArrayRequest(Request.Method.GET,url,null, Response.Listener { response ->
+
+            for(x in 0..response.length()-1)
+                list.add(Item(response.getJSONObject(x).getInt("id"),response.getJSONObject(x).getString("name"),
+                    response.getJSONObject(x).getString("price"),response.getJSONObject(x).getString("photo")))
+
+            var adp=ItemAdapter(this,list)
+            item_rv.layoutManager= LinearLayoutManager(this)
+            item_rv.adapter=adp
+
+        },Response.ErrorListener { error ->
+            Toast.makeText(this,error.message,Toast.LENGTH_LONG).show()
+        })
+
+        rq.add(jar)
 
     }
 }
